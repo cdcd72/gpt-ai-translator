0c2d637704a1f07908c05021c0bb34e7743a6252
cdcd72 <cdcd71517@gmail.com>
2025-06-21 23:50:15 +0800

    1. 文字轉語音改使用 OpenAI 的 TTS 相關模型 2. 改使用 tinytag 解析語音時間並移除呼叫 FFMPEG API

--- api/ai/chatgpt.py
+++ api/ai/chatgpt.py
@@ -12,13 +12,9 @@ class ChatGPT:
     def __init__(self):
         self.model = os.getenv("OPENAI_COMPLETION_MODEL", "gpt-4.1-mini")
         self.temperature = float(os.getenv("OPENAI_COMPLETION_TEMPERATURE", "0.2"))
-
-    def whisper(self, audio_path):
-        with open(audio_path, "rb") as audio_file:
-            transcript = client.audio.transcriptions.create(
-                model="whisper-1", file=audio_file
-            )
-        return transcript.text
+        self.tts_model = os.getenv("OPENAI_TTS_MODEL", "gpt-4o-mini-tts")
+        self.tts_voice = os.getenv("OPENAI_TTS_VOICE", "alloy")
+        self.whisper_model = os.getenv("OPENAI_WHISPER_MODEL", "whisper-1")
 
     def translate(self, text, language):
         prompt = f"""
@@ -32,3 +28,16 @@ Help me translate this sentence to {language}, only target language, no need ori
             temperature=self.temperature,
         )
         return response.choices[0].message.content
+
+    def tts(self, text, audio_path):
+        with client.audio.speech.with_streaming_response.create(
+            model=self.tts_model, voice=self.tts_voice, input=text
+        ) as response:
+            response.stream_to_file(audio_path)
+
+    def whisper(self, audio_path):
+        with open(audio_path, "rb") as audio_file:
+            transcript = client.audio.transcriptions.create(
+                model=self.whisper_model, file=audio_file
+            )
+        return transcript.text
